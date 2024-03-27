776ff02614c1598ec89869ad69145294aa5278a8
D-Kamunya <kinc.developer@gmail.com>
2024-03-27 12:17:51 +0300

    Ch: Make default rent invoice type name uneditable

--- public/assets/js/custom/invoice-type.js
+++ public/assets/js/custom/invoice-type.js
@@ -1,20 +1,25 @@
 $(function () {
-    'use strict'
-    $('#add').on('click', function () {
-        var selector = $('#addInvoiceTypeModal');
-        selector.find('.is-invalid').removeClass('is-invalid');
-        selector.find('.error-message').remove();
-        selector.modal('show')
-        selector.find('form').trigger('reset');
+    "use strict";
+    $("#add").on("click", function () {
+        var selector = $("#addInvoiceTypeModal");
+        selector.find(".is-invalid").removeClass("is-invalid");
+        selector.find(".error-message").remove();
+        selector.modal("show");
+        selector.find("form").trigger("reset");
     });
 
-    $(document).on('click', '.edit', function (e) {
-        var selector = $('#editInvoiceTypeModal');
-        selector.find('.is-invalid').removeClass('is-invalid');
-        selector.find('.error-message').remove();
-        selector.modal('show')
-        selector.find('input[name=id]').val($(this).data('item').id)
-        selector.find('input[name=name]').val($(this).data('item').name)
-        selector.find('input[name=tax]').val(parseFloat($(this).data('item').tax).toFixed(2))
-    })
-})
+    $(document).on("click", ".edit", function (e) {
+        var selector = $("#editInvoiceTypeModal");
+        selector.find(".is-invalid").removeClass("is-invalid");
+        selector.find(".error-message").remove();
+        selector.modal("show");
+        selector.find("input[name=id]").val($(this).data("item").id);
+        selector.find("input[name=name]").val($(this).data("item").name);
+        selector
+            .find("input[name=tax]")
+            .val(parseFloat($(this).data("item").tax).toFixed(2));
+        selector.find("input[name=name]").val() == "Rent"
+            ? selector.find("input[name=name]").prop("readonly", true)
+            : "";
+    });
+});
