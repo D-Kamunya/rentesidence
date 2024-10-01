c793e9299928734a944033349a18fb882445e68f
D-Kamunya <kinc.developer@gmail.com>
2024-10-01 13:17:40 +0300

    ft: Add frontend feature to pay tenant invoices via mpesa transaction code

--- public/assets/js/custom/tenant-invoice-pay.js
+++ public/assets/js/custom/tenant-invoice-pay.js
@@ -5,11 +5,36 @@ $("#bank_id").on("change", function () {
 });
 
 $(document).on("click", ".paymentGateway", function () {
-    $("#selectGateway").val($(this).data("gateway").replace(/\s+/g, ""));
-    if ($("#selectGateway").val() == "mpesa") {
-        $("#mpesa_account_id").attr("required", true);
-    } else {
+    var selectGateway = $(this).data("gateway").replace(/\s+/g, "");
+    $("#selectGateway").val(selectGateway);
+    if (selectGateway == "bank") {
+        $("#payBtn").removeClass("d-none");
+        $("#bank_slip").attr("required", true);
+        $("#bank_id").attr("required", true);
+        $("#gatewayCurrencyAmount").text("");
+        $("#mpesaGatewayCurrencyAmount").text("");
+        $("#mpesaPayBtn").addClass("d-none");
         $("#mpesa_account_id").attr("required", false);
+        $("#mpesaAccountAppend").addClass("d-none");
+    } else if (selectGateway == "mpesa") {
+        $("#mpesa_selectGateway").val(selectGateway);
+        $("#mpesa_selectCurrency").val("");
+        $("#mpesaAccountAppend").removeClass("d-none");
+        $("#mpesaPayBtn").removeClass("d-none");
+        $("#gatewayCurrencyAmount").text("Via STK");
+        $("#mpesaGatewayCurrencyAmount").text("");
+        $("#mpesa_account_id").attr("required", true);
+        $("#bank_slip").attr("required", false);
+        $("#bank_id").attr("required", false);
+        $("#bankAppend").addClass("d-none");
+    } else {
+        $("#bank_slip").attr("required", false);
+        $("#payBtn").removeClass("d-none");
+        $("#bank_id").attr("required", false);
+        $("#mpesa_account_id").attr("required", false);
+        $("#gatewayCurrencyAmount").text("");
+        $("#mpesaGatewayCurrencyAmount").text("");
+        $("#mpesaPayBtn").addClass("d-none");
     }
     $("#selectCurrency").val("");
     commonAjax(
@@ -50,9 +75,18 @@ function getCurrencyRes(response) {
 }
 
 $(document).on("click", ".gatewayCurrencyAmount", function () {
+    var gateway = $("#selectGateway").val();
     var getCurrencyAmount = "(" + $(this).find("input").val() + ")";
     $("#gatewayCurrencyAmount").text(getCurrencyAmount);
+    if (gateway === "mpesa") {
+        $("#gatewayCurrencyAmount").text("Via STK " + getCurrencyAmount);
+        $("#mpesaGatewayCurrencyAmount").text(getCurrencyAmount);
+        document.getElementById("mpesa-amount").textContent = getCurrencyAmount;
+    } else {
+        $("#gatewayCurrencyAmount").text(getCurrencyAmount);
+    }
     $("#selectCurrency").val($(this).text().replace(/\s+/g, ""));
+    $("#mpesa_selectCurrency").val($(this).text().replace(/\s+/g, ""));
 });
 
 function showMpesaPreloader() {
@@ -96,5 +130,61 @@ $("#payBtn").on("click", function () {
                 }
             }
         }
+    }
+});
+
+$("#mpesaPayBtn").on("click", function () {
+    var gateway = $("#selectGateway").val();
+    var currency = $("#selectCurrency").val();
+    if (gateway == "") {
+        toastr.error("Select Gateway");
+        $("#mpesaPayBtn").attr("type", "button");
+    } else {
+        if (currency == "") {
+            toastr.error("Select Currency");
+            $("#mpesaPayBtn").attr("type", "button");
+        } else {
+            var invoice_form = document.getElementById("pay-invoice-form");
+            invoice_form.action = "";
+
+            $("#mpesaPayBtn").attr("type", "submit");
+
+            // Prevent default form submission
+            invoice_form.addEventListener("submit", function (event) {
+                event.preventDefault();
+            });
+
+            if (invoice_form.checkValidity()) {
+                var selector = $("#mpesaCodePaymentMethodModal");
+                selector.modal("show");
+            }
+        }
+    }
+});
+
+$(document).on("change", "#mpesa_account_id", function () {
+    var selectedOption = $(this).find("option:selected");
+    var textContent = selectedOption.text().trim();
+    var details = selectedOption.data("details");
+    if (details === "TILLNUMBER") {
+        var tillNumber = "";
+        var parts = textContent.split("- Till Number: ");
+        if (parts.length > 1) {
+            tillNumber = parts[1].trim();
+        }
+        document.getElementById("till-number").textContent = tillNumber;
+        $("#mpesa-code-payment-paybill").addClass("d-none");
+        $("#mpesa-code-payment-till").removeClass("d-none");
+    } else if (details === "PAYBILL") {
+        // Parse Paybill and Account Name
+        const paybillMatch = textContent.match(/Paybill:\s*([\w\d]+)/);
+        const accountNameMatch = textContent.match(/Account Name:\s*([\w\d]+)/);
+        // Extract values
+        const paybill = paybillMatch ? paybillMatch[1] : "";
+        const accountName = accountNameMatch ? accountNameMatch[1] : "";
+        document.getElementById("bs-number").textContent = paybill;
+        document.getElementById("acc-number").textContent = accountName;
+        $("#mpesa-code-payment-paybill").removeClass("d-none");
+        $("#mpesa-code-payment-till").addClass("d-none");
     }
 });
--- resources/views/tenant/invoices/pay.blade.php
+++ resources/views/tenant/invoices/pay.blade.php
@@ -224,11 +224,79 @@
                                         <button type="button" class="theme-btn me-2 mb-1 w-100"
                                             id="payBtn">{{ __('Pay Now') }}
                                             <span class="ms-1" id="gatewayCurrencyAmount"></span></button>
+                                        
+                                        <button type="button" class="d-none theme-btn me-2 mb-1 w-100"
+                                            id="mpesaPayBtn">{{ __('Pay Now Via Mpesa Code') }}
+                                            <span class="ms-1" id="mpesaGatewayCurrencyAmount"></span></button>
                                     </div>
                                 </form>
                             </div>
                         </div>
                         <!-- Tenant Portal Invoice Details Right Side End -->
+
+                        <!-- Mpesa Transaction Code Payment Method Modal Start -->
+                        <div class="modal fade big-modal" id="mpesaCodePaymentMethodModal" tabindex="-1" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header border-0 p-0">
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><span
+                                                class="iconify" data-icon="akar-icons:cross"></span></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <!-- Choose a plan content Start -->
+                                        <div class="payment-method-area">
+                                            <h2 class="text-center payment-method-area-title">{{ __('Mpesa Payment Details') }}</h2>
+                                            <div class="payment-method-wrap px-5">
+                                                <form id="" class="" action="{{ route('payment.checkout') }}" method="POST"
+                                                    enctype="multipart/form-data">
+                                                    @csrf
+                                                    <input type="hidden" id="mpesa_code_account_id" name="mpesa_account_id" value="">
+                                                    <input type="hidden" id="mpesa_selectGateway" name="gateway">
+                                                    <input type="hidden" id="mpesa_selectCurrency" name="currency">
+                                                    <input type="hidden"  id="mpesa_default-currency" name="default-currency" value="{{ json_encode(session('default_currency')) }}">
+                                                    <div class="row">
+                                                        <h4>Hello {{ auth()->user()->name }}</h4>
+                                                        <b>Follow the instructions below to make payment from your MPESA account.</b>
+                                                        <ol>
+                                                            <li>Go to the Safaricom SIM Tool Kit</li>
+                                                            <li>Select the MPESA menu</li>
+                                                            <li>Select Lipa na MPESA</li>
+                                                            <div class="" id="mpesa-code-payment-paybill">
+                                                                <li>Select Pay bill</li>
+                                                                <li>Select Enter Business Number, and enter <strong id="bs-number">111739</strong></li>
+                                                                <li>Select Enter Account Number and type <strong id="acc-number"></strong></li>
+                                                            </div>
+                                                            <div class="" id="mpesa-code-payment-till">
+                                                                <li>Select Buy Goods and Services</li>
+                                                                <li>Enter Till Number <strong id="till-number"></strong></li>
+                                                            </div>
+                                                            
+                                                            <li>Enter your subscription amount <strong id="mpesa-amount"></strong></li>
+                                                            <li>Enter your MPESA pin number</li>
+                                                        </ol>
+                                                    </div>
+                                                    <div class="col-md-12 mb-25">
+                                                        <label
+                                                            class="label-text-title color-heading font-medium mb-2">{{ __('Enter Mpesa Transaction Code') }}</label>
+                                                        <input type="text" id="mpesaTransactionCode" name="mpesa_transaction_code" class="form-control" placeholder="{{__('Mpesa Transaction Code')}}">
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-md-12 text-center">
+                                                            <button type="button" class="theme-btn me-2 mb-1 w-75"
+                                                                id="mpesaCodeSubmitBtn">{{ __('Submit Code') }}
+                                                                </button>
+                                
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <!-- Choose a plan content End -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Mpesa Transaction Code Payment Method Modal End -->
                     </div>
                 </div>
             </div>
