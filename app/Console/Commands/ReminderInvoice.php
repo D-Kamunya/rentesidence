d3d1ba47e2f3e34696f456ac94249e7ab282753b
D-Kamunya <kinc.developer@gmail.com>
2025-01-30 17:34:05 +0300

    feat: add overdue invoices reminder

--- app/Console/Commands/ReminderInvoice.php
+++ app/Console/Commands/ReminderInvoice.php
@@ -12,113 +12,79 @@ use Illuminate\Support\Facades\Log;
 
 class ReminderInvoice extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'reminder:invoice';
+    protected $description = 'Reminder invoice for tenant';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'reminder invoice for tenant';
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
         try {
-            if (getOption('remainder_status', 0) == REMAINDER_STATUS_ACTIVE) {
-                $mailService = new MailService;
-                $invoices = Invoice::query()
-                    ->where('status', INVOICE_STATUS_PENDING)
-                    ->get();
+            if (getOption('remainder_status', 0) != REMAINDER_STATUS_ACTIVE) {
+                throw new Exception('Reminder status inactive');
+            }
 
-                if (getOption('remainder_everyday_status') == REMAINDER_EVERYDAY_STATUS_ACTIVE) {
-                    foreach ($invoices as $invoice) {
-                        if ($invoice->due_date >= today()) {
-                            if (getOption('send_email_status', 0) == ACTIVE) {
-                                $emails = [$invoice->tenant->user->email];
-                                $subject =  __('Payment reminder') . ' ' . $invoice->invoice_no . ' ' . __('due on date') . ' '  . $invoice->due_date;
-                                $title =  __('Payment reminder!');
-                                $message = __('You have a due invoice');
-                                $ownerUserId = $invoice->owner_user_id;
-                                $amount = $invoice->amount;
-                                $dueDate = $invoice->due_date;
-                                $month = $invoice->month;
-                                $invoiceNo = $invoice->invoice_no;
-                                $status = __('Pending');
+            $mailService = new MailService;
+            $invoices = Invoice::where('status', INVOICE_STATUS_PENDING)->get();
 
-                                // send mail
-                                $template = EmailTemplate::where('owner_user_id', $ownerUserId)->where('category', EMAIL_TEMPLATE_INVOICE)->where('status', ACTIVE)->first();
-                                if ($template) {
-                                    $customizedFieldsArray = [
-                                        '{{amount}}' => $invoice->amount,
-                                        '{{due_date}}' => $invoice->due_date,
-                                        '{{month}}' => $invoice->month,
-                                        '{{invoice_no}}' => $invoice->invoice_no,
-                                        '{{app_name}}' => getOption('app_name')
-                                    ];
-                                    $content = getEmailTemplate($template->body, $customizedFieldsArray);
-                                    $mailService->sendCustomizeMail($emails, $template->subject, $content);
-                                } else {
-                                    $mailService->sendInvoiceMail($ownerUserId, $status, $emails, $subject, $message,  $title, $amount, $dueDate, $month, $invoiceNo);
-                                }
-                            }
-                        }
-                    }
+            $sendEveryday = getOption('remainder_everyday_status') == REMAINDER_EVERYDAY_STATUS_ACTIVE;
+            $reminderDays = explode(',', getOption('reminder_days'));
+            $sendEverydayOverDue = getOption('OVERDUE_REMAINDER_EVERYDAY_STATUS') == REMAINDER_EVERYDAY_STATUS_ACTIVE;
+            $reminderDaysOverDue = explode(',', getOption('OVERDUE_REMINDER_DAYS'));
+            $sendEmail = getOption('send_email_status', 0) == ACTIVE;
+
+            foreach ($invoices as $invoice) {
+                $dueDate = Carbon::parse($invoice->due_date);
+                $diffDay = $dueDate->diffInDays(today());
+
+                if ($sendEveryday && $dueDate >= today()) {
+                    $this->sendReminder($mailService, $invoice, $sendEmail);
+                } elseif (!$sendEveryday && in_array($diffDay, $reminderDays) && $dueDate >= today()) {
+                    $this->sendReminder($mailService, $invoice, $sendEmail);
                 }
-
-                $reminderDaysArray = explode(',', getOption('reminder_days'));
-                if (getOption('remainder_everyday_status') != REMAINDER_EVERYDAY_STATUS_ACTIVE) {
-                    foreach ($invoices as $invoice) {
-                        if ($invoice->due_date >= today()) {
-                            $from_date = Carbon::parse($invoice->due_date);
-                            $diffDay = $from_date->diffInDays(today());
-                            if (in_array($diffDay, $reminderDaysArray)) {
-                                if (getOption('send_email_status', 0) == ACTIVE) {
-                                    $emails = [$invoice->tenant->user->email];
-                                    $subject =  __('Payment reminder') . ' ' . $invoice->invoice_no . ' ' . __('due on date') . ' '  . $invoice->due_date;
-                                    $title =  __('Payment reminder!');
-                                    $message = __('You have a due invoice');
-                                    $ownerUserId = $invoice->owner_user_id;
-                                    $amount = $invoice->amount;
-                                    $dueDate = $invoice->due_date;
-                                    $month = $invoice->month;
-                                    $invoiceNo = $invoice->invoice_no;
-                                    $status = __('Pending');
-
-                                    // send mail
-                                    $template = EmailTemplate::where('owner_user_id', $ownerUserId)->where('category', EMAIL_TEMPLATE_INVOICE)->where('status', ACTIVE)->first();
-                                    if ($template) {
-                                        $customizedFieldsArray = [
-                                            '{{amount}}' => $invoice->amount,
-                                            '{{due_date}}' => $invoice->due_date,
-                                            '{{month}}' => $invoice->month,
-                                            '{{invoice_no}}' => $invoice->invoice_no,
-                                            '{{app_name}}' => getOption('app_name')
-                                        ];
-                                        $content = getEmailTemplate($template->body, $customizedFieldsArray);
-                                        $mailService->sendCustomizeMail($emails, $template->subject, $content);
-                                    } else {
-                                        $mailService->sendInvoiceMail($ownerUserId, $status, $emails, $subject, $message, $title, $amount, $dueDate, $month, $invoiceNo);
-                                    }
-                                }
-                            }
-                        }
-                    }
+                if ($sendEverydayOverDue && $dueDate <= today()) {
+                    $this->sendReminder($mailService, $invoice, $sendEmail,true);
+                } elseif (!$sendEverydayOverDue && in_array($diffDay, $reminderDaysOverDue) && $dueDate <= today()) {
+                    $this->sendReminder($mailService, $invoice, $sendEmail,true);
                 }
-            } else {
-                throw new Exception('reminder status inactive');
             }
         } catch (Exception $e) {
-            Log::info('auto reminder error: ' . $e->getMessage());
+            Log::info('Auto reminder error: ' . $e->getMessage());
+        }
+    }
+
+    private function sendReminder($mailService, $invoice, $sendEmail,$overDue=false)
+    {
+        if (!$sendEmail) {
+            return;
+        }
+
+        $emails = [$invoice->tenant->user->email];
+        $subject = $overDue ? __('Payment reminder') . ' ' . $invoice->invoice_no . ' ' . __('overdue on date') . ' ' . $invoice->due_date :  __('Payment reminder') . ' ' . $invoice->invoice_no . ' ' . __('due on date') . ' ' . $invoice->due_date;
+        $title = __('Payment reminder!');
+        $message = $overDue ? __('You have an overdue invoice'): __('You have a due invoice');
+        $ownerUserId = $invoice->owner_user_id;
+        $amount = $invoice->amount;
+        $dueDate = $invoice->due_date;
+        $month = $invoice->month;
+        $invoiceNo = $invoice->invoice_no;
+        $status = __('Pending');
+
+        $template = EmailTemplate::where('owner_user_id', $ownerUserId)
+            ->where('category', EMAIL_TEMPLATE_INVOICE)
+            ->where('status', ACTIVE)
+            ->first();
+
+        if ($template) {
+            $customizedFieldsArray = [
+                '{{amount}}' => $amount,
+                '{{due_date}}' => $dueDate,
+                '{{month}}' => $month,
+                '{{invoice_no}}' => $invoiceNo,
+                '{{app_name}}' => getOption('app_name')
+            ];
+            $content = getEmailTemplate($template->body, $customizedFieldsArray);
+            $mailService->sendCustomizeMail($emails, $template->subject, $content);
+        } else {
+            $mailService->sendInvoiceMail($ownerUserId, $status, $emails, $subject, $message, $title, $amount, $dueDate, $month, $invoiceNo);
         }
     }
 }
