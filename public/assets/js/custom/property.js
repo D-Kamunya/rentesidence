5f52ca2a2f43bfd556abfec82591d972ee328288
D-Kamunya <kinc.developer@gmail.com>
2025-03-28 10:11:31 +0300

    fix: fix property image upload

--- public/assets/js/custom/property.js
+++ public/assets/js/custom/property.js
@@ -182,39 +182,6 @@ function getCitiesByState(state_id) {
         { state_id: state_id }
     );
 }
-document
-    .querySelector("#default-profile-img-file-input")
-    .addEventListener("change", function () {
-        console.log("de");
-        var o = document.querySelector(".default-user-profile-image"),
-            e = document.querySelector(".default-profile-img-file-input")
-                .files[0],
-            i = new FileReader();
-        i.addEventListener(
-            "load",
-            function () {
-                o.src = i.result;
-            },
-            !1
-        ),
-            e && i.readAsDataURL(e);
-    });
-
-document
-    .querySelector("#default-unit-img-file-input")
-    .addEventListener("change", function () {
-        var o = document.querySelector(".default-unit-profile-image"),
-            e = document.querySelector(".default-unit-img-file-input").files[0],
-            i = new FileReader();
-        i.addEventListener(
-            "load",
-            function () {
-                o.src = i.result;
-            },
-            !1
-        ),
-            e && i.readAsDataURL(e);
-    });
 
 $(document).on("click", ".unit-edit", function () {
     let detailsUrl = $(this).data("detailsurl");
--- resources/views/owner/property/all-unit-list.blade.php
+++ resources/views/owner/property/all-unit-list.blade.php
@@ -788,4 +788,38 @@
     @include('common.layouts.datatable-script')
     <script src="{{ asset('assets/js/pages/alldatatables.init.js') }}"></script>
     <script src="{{ asset('assets/js/custom/property.js') }}"></script>
+    <script>
+        document
+            .querySelector("#default-profile-img-file-input")
+            .addEventListener("change", function () {
+                var o = document.querySelector(".default-user-profile-image"),
+                    e = document.querySelector(".default-profile-img-file-input")
+                        .files[0],
+                    i = new FileReader();
+                i.addEventListener(
+                    "load",
+                    function () {
+                        o.src = i.result;
+                    },
+                    !1
+                ),
+                    e && i.readAsDataURL(e);
+            });
+
+        document
+            .querySelector("#default-unit-img-file-input")
+            .addEventListener("change", function () {
+                var o = document.querySelector(".default-unit-profile-image"),
+                    e = document.querySelector(".default-unit-img-file-input").files[0],
+                    i = new FileReader();
+                i.addEventListener(
+                    "load",
+                    function () {
+                        o.src = i.result;
+                    },
+                    !1
+                ),
+                    e && i.readAsDataURL(e);
+            });
+    </script>
 @endpush
